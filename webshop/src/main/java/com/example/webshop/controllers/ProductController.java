fcc69fabd038426cea7b57368b786e5779efec44
Teszike99 <teszidani02@gmail.com>
2023-02-06 23:12:43 +0100

    Feature - product controller created

--- webshop/src/main/java/com/example/webshop/controllers/ProductController.java
+++ webshop/src/main/java/com/example/webshop/controllers/ProductController.java
@@ -1,9 +1,19 @@
 package com.example.webshop.controllers;
 
+import com.example.webshop.models.Product;
+import com.example.webshop.services.ProductServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProductController {
-
+    @Autowired
+    ProductServices productServices;
+    @GetMapping("/product")
+    public List<Product> getProduct(){
+        return productServices.getProduct();
+    }
 }
