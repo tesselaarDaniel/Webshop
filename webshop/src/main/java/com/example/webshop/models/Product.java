45296649d4a663f6f90306463b4db2cd18e034ae
Teszike99 <teszidani02@gmail.com>
2023-02-06 23:11:59 +0100

    Feature - Model layer and product model created

--- webshop/src/main/java/com/example/webshop/models/Product.java
+++ webshop/src/main/java/com/example/webshop/models/Product.java
@@ -1,9 +1,53 @@
 package com.example.webshop.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
     private Integer id;
     private String name;
     private Integer quantity;
     private String type;
 
+    public Product(String name, Integer quantity, String type) {
+        this.name = name;
+        this.quantity = quantity;
+        this.type = type;
+    }
+
+    public Product() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
+
