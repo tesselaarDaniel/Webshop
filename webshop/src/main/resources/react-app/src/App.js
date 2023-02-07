424c0e80592e5f310fefa012890ec4601d3b5e4b
Teszike99 <teszidani02@gmail.com>
2023-02-07 13:36:24 +0100

    Test - testing material UI

--- webshop/src/main/resources/react-app/src/App.js
+++ webshop/src/main/resources/react-app/src/App.js
@@ -3,20 +3,45 @@ import './App.css';
 import {apiGet} from "./DataHandler";
 import {useEffect} from "react";
 import {useState} from "react";
+import {Box, Container, Typography, Paper} from "@mui/material";
 
+const serviceList = ["Service1", "Service2", "Service3"];
 function App() {
-    const [data, setData] = useState([])
-    const productList = data.map( product => product.name)
-    useEffect(() => {
-        apiGet(`/product`)
-            .then(r => setData(r))
-    }, [])
-    console.log(data)
   return (
-    <div className="App">
-        <h1>{productList}</h1>
-    </div>
-  );
+      <Container>
+
+          <Container>
+              <Typography variant="h1" sx={{my: 4, textAlign: "center", color: "green"}}>
+                  Title
+              </Typography>
+              <Typography variant="h2">
+                  hello
+                  <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", gap: 3}}>
+                      <Typography variant="h2" >col1</Typography>
+                      <Typography variant="h2" >col2</Typography>
+                      <Typography variant="h2" >col3</Typography>
+                      <Typography variant="h2" >col4</Typography>
+                      <Typography variant="h2" >col4</Typography>
+                  </Box>
+                  <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", gap: 4}}>
+                      {serviceList.map((service) => (
+                          <Paper elevation={3}>
+                              <Box sx={{m: 2}}>
+                                  <Typography variant="h3">{service}</Typography>
+                                  <Typography sx={{ mt: 8 }}>
+                                      ajsdljlajsdlfjaslkdjflasjdlfjasldfjalsdjflasjdf
+                                      asldjflasjdlfjasldfjlasdjflasjdlfjalsdjflajsdlfja
+                                      alsjdflasjldfjalskdjflaksjdlfjaslkdjflakjsdlkfjalsdjf
+                                  </Typography>
+                              </Box>
+                          </Paper>
+                      ))}
+                  </Box>
+              </Typography>
+          </Container>
+
+      </Container>
+  )
 }
 
 export default App;
