34fc194482f222a82af9b236cdefd26e91537384
Teszike99 <teszidani02@gmail.com>
2023-02-09 08:15:12 +0100

    Feature - basic app design

--- webshop/src/main/resources/react-app/src/js/App.js
+++ webshop/src/main/resources/react-app/src/js/App.js
@@ -1,10 +1,18 @@
-import '../Design/App.css';
+import '../design/App.css';
 
 import HomePage from "./HomePage";
+import {Container, Typography} from "@mui/material";
 
 function App() {
   return (
-     <HomePage/>
+      <div className="main-container">
+          <div className="header-container">
+              <h1>My anime list</h1>
+          </div>
+          <div className="content">
+              <HomePage/>
+          </div>
+      </div>
   )
 }
 
